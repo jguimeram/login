f3696a5deba2323eccdf48593762cb98bfa49463
jguimeram <jguimera54@gmail.com>
2025-08-24 10:47:41 +0200

    css link fixed

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    ../views/layouts/header.php
-    <link rel="stylesheet" href=<?php echo __DIR__ . "/../../public/assets/style.css" ?>>
+    <link rel="stylesheet" href="/assets/style.css">
     <title>Authentication</title>
 </head>
